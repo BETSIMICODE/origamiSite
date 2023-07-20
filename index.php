1f64fbf680927e767f9891f0b0c3dd7f437cc9c8
unknown <zanajaona2404@gmail.com>
2023-07-20 16:59:19 +0200

    update update site

--- index.php
+++ index.php
@@ -1,4 +1,291 @@
-<!Doctype html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="views/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>OrigamiTech</title>
+</head>
+<body>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
+    <div class="bg1">
+        <div class="container">
+            <nav class="navContent">
+                <img class="logo" src="views/images/logo.png" alt="Logo OrigamiTech">
+                <div class="nav">
+                    <img src="views/images/x.png" alt="Bouton quitter liste de navigation" class="btnExitNavMobile">
+                    <a class="btnNav btnHome" href="">
+                        Accueil
+                    </a>
+                    <a class="btnNav btnService"  href="" >
+                        Services
+                    </a>
+                    <a class="btnNav btnEvent"  href="">
+                        Evènement
+                    </a>
+                    <a class="btnNav btnContact"  href="">
+                        Contact
+                    </a>
+                </div>
+                <img class="btnNavMobile" src="views/images/menu.png" alt="Bouton menu pour mobile">
+            </nav>
+            <div class="middleContent">
+                <div class="leftContent">
+                    <h1 class="titleOrigami">
+                        Transformation digitale
+                    </h1>
+                    <p class="descriOrigami">
+                        Origami tech est une agence numérique à Fianarantsoa Isaha. Notre service sont: 
+                        création d'un site web, couverture médiatique et évènementiel...
+                    </p>
+                    <button class="btnStart">Get started</button>
+    
+                </div>
+                <div class="rightContent">
+                    <div class="containerServiceContain">
+                        <hr class="lineWhite">
+                        <div class="serviceContent">
+                            <div class="titleServiceContent">
+                                <h2 class="titleService">
+                                    Création site web
+                                </h2>
+                            </div>
+                            
+                            <div class="imageServiceContent">
+                                <img class="imgServ" src="views/images/test1.png" alt="">
+                                <img class="imgServ" src="views/images/test2.jpg" alt="">
+                                <img class="imgServ" src="views/images/test3.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="serviceContent">
+                            <div class="titleServiceContent">
+                                <h2 class="titleService">
+                                    Evenement
+                                </h2>
+                            </div>
+                            <div class="imageServiceContent">
+                                <img class="imgServ" src="views/images/test1.png" alt="">
+                                <img class="imgServ" src="views/images/test2.jpg" alt="">
+                                <img class="imgServ" src="views/images/test3.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="serviceContent">
+                            <div class="titleServiceContent">
+                                <h2 class="titleService">
+                                    Couverture Mediatique
+                                </h2>
+                            </div>
+                            <div class="imageServiceContent">
+                                <img class="imgServ" src="views/images/test1.png" alt="">
+                                <img class="imgServ" src="views/images/test2.jpg" alt="">
+                                <img class="imgServ" src="views/images/test3.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="radioContent">
+                        <div class="radioSlide radioChecked"> </div>
+                        <div class="radioSlide"></div>
+                        <div class="radioSlide"></div>
+                    </div>
+    
+                </div>
+            </div>
+            <div class="contactContent">
+                <img class="contactIcone round" src="views/images/fb.png" alt="icone facebook">
+                <img class="contactIcone square" src="views/images/lkdin.png" alt="icone gmail">
+            </div>
+    
+        </div>
+    </div>
+    <div class="bg2">
+        <h1 class="titleSecNoservice">
+            Nos services
+        </h1>
+        <div class="wrapper">
+            <div class="box">
+                <img src="views/images/web.png" alt="Photo creation site web">
+                <div class="description">
+                    <h2>Création site web</h2>
+                    <div class="btnStart moreService">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Voir plus
+                    </div>
+                </div>
+            </div>
+            <div class="box">
+                <img src="views/images/evenement.png" alt="Photo couverture médiatique">
+                <div class="description">
+                    <h2>Couverture médiatique</h2>
+                    <div class="btnStart moreService">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Voir plus
+                    </div>
+                </div>
+            </div>
+            <div class="box">
+                <img src="views/images/creation.png" alt="Photo créateur digitale">
+                <div class="description">
+                    <h2>Créateur digitale</h2>
+                    <div class="btnStart moreService">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Voir plus
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg3">
+        
+        <h1 class="titleSecNoservice titleSecEvent">Evènements</h1>
+       
+        <div class="eventContent">
+            
+        </div>
+       
+    </div>
+    <div class="bg4">
+        <div class="titleTrustContent">
+            <h1 class="titleTrust">Confiez-vous à nous!</h1>
+        </div>
+       
+    </div>
+    <div class="bg5">
+        
+        <h1 class="titleSecNoservice titleSecMember">Notre equipe</h1>
+       
+        <div class="memberContact">
+            <div class="card">
+                <div class="content">
+                    <div class="img"><img src="views/images/princy.jpg" alt="Photo de couverture CTO"></div>
+                    <div class="cardContent">
+                        <h3>Sanders RANDRIANARISON<br><span>Directeur Technique</span></h3>
+                    </div>
+                </div>
+                <ul class="sci">
+                    <li style="--i:1">
+                        <a href="#"><img class="iconMember roundIconMember" src="views/images/fb.png" alt="Icone Facebook"></a>
+                    </li>
+                    <li style="--i:2">
+                        <a href="#"><img class="iconMember squareIconMember" src="views/images/lkdin.png" alt="Icone Linkedin"></a>
+                    </li>
+                    <li style="--i:3">
+                        <a href="#"><img class="iconMember squareIconMember" src="views/images/insta.png" alt="Icon Instagram"></a>
+                    </li>
+                </ul>
+            </div>
 
-</html>
+            <div class="card">
+                <div class="content">
+                    <div class="img"><img  src="views/images/andry.jpg" alt="Photo de couverture RH"></div>
+                    <div class="cardContent">
+                        <h3>Ando RABE<br><span>Directrice Ressource Humaine</span></h3>
+                    </div>
+                </div>
+                <ul class="sci">
+                    <li style="--i:1">
+                        <a href="#"><img class="iconMember roundIconMember" src="views/images/fb.png" alt="Icone Facebook"></a>
+                    </li>
+                    <li style="--i:2">
+                        <a href="#"><img class="iconMember squareIconMember" src="views/images/lkdin.png" alt=""></a>
+                    </li>
+                    <li style="--i:3">
+                        <a href="#"><img class="iconMember squareIconMember" src="views/images/insta.png" alt=""></a>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div class="card">
+                <div class="content">
+                    <div class="img"><img  src="views/images/almadie.jpg" alt="Photo de couverture Admin"></div>
+                    <div class="cardContent">
+                        <h3>Almadie ABDOUl<br><span>Responsable administrative</span></h3>
+                    </div>
+                </div>
+                <ul class="sci">
+                    <li style="--i:1">
+                        <a href="#"><img class="iconMember roundIconMember" src="views/images/fb.png" alt="Icone Facebook"></a>
+                    </li>
+                    <li style="--i:2">
+                        <a href="#"><img class="iconMember squareIconMember" src="views/images/lkdin.png" alt=""></a>
+                    </li>
+                    <li style="--i:3">
+                        <a href="#"><img class="iconMember squareIconMember" src="views/images/insta.png" alt=""></a>
+                    </li>
+                </ul>
+            </div>
+                <div class="card">
+                    <div class="content">
+                    <a href="https://mahafeno-git-main-ztsirionantsoa.vercel.app/">
+                        <div class="img"><img src="views/images/doughlas2.jpg" alt="Photo de couverture Developpeur"></div>
+                        <div class="cardContent">
+                        <h3>Doughlas TSIRIONANTSOA<br><span>Responsable Informatique</span></h3>
+                    </a>
+                    </div>
+                </div>
+                <ul class="sci">
+                    <li style="--i:1">
+                        <a href="#"><img class="iconMember roundIconMember" src="views/images/fb.png" alt="Icone Facebook"></a>
+                    </li>
+                    <li style="--i:2">
+                        <a href="#"><img class="iconMember squareIconMember" src="views/images/lkdin.png" alt="Icone Linkedin"></a>
+                    </li>
+                    <li style="--i:3">
+                        <a href="#"><img class="iconMember squareIconMember" src="views/images/insta.png" alt="Icone Instagram"></a>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+
+    </div>
+
+
+
+    <footer class="copyright-footer">
+    <div class="footer-bottom">
+                <p>&copy; 2023 OrigamiTech. Tous droits réservés.</p>
+            </div>
+</footer>
+<style>
+    .footer {
+            background-color: #333;
+            padding: 20px 0;
+            color: #fff;
+            text-align: center;
+        }
+
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+
+        .footer-content {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 10px;
+        }
+
+        
+
+        .footer-bottom {
+            font-size: 25px;
+        }
+    </style>
+
